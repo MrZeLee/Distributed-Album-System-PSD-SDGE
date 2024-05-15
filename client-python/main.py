2529fae04bacfef92ae6d5af60951a46e184ba0f
José Luis Moura <joseluismoura98@gmail.com>
2024-05-15 19:40:14 +0100

    feat(main.py): add json import and zmq_connect_queue for enhanced message handling
    refactor(main.py): modify tcp_receive_thread to use zmq_connect_queue based on zmq flag
    
    feat(main.py): replace zmq_dealer_thread with zmq_pub_thread for publishing messages
    
    feat(main.py): replace zmq_router_thread with zmq_sub_thread for subscribing to messages
    
    feat(main.py): add zmq_connect function to manage dynamic ZeroMQ connections
    
    feat(main.py): implement extract_routers to parse router info from JSON
    
    refactor(main.py): update zmq_url to use 127.0.0.1 for consistency
    
    feat(main.py): bind and set options for ZeroMQ PUB and SUB sockets in main function
    
    refactor(main.py): update thread initialization to use new zmq_pub_thread and zmq_sub_thread
    
    refactor(main.py): enhance socket closure process with ZeroMQ context termination and error handling

--- client-python/main.py
+++ client-python/main.py
@@ -3,11 +3,13 @@ import queue
 import socket
 import zmq
 import sys
+import json
 
 # Global queues for inter-thread communication
 message_queue = queue.Queue()
 tcp_send_queue = queue.Queue()
 zmq_send_queue = queue.Queue()
+zmq_connect_queue = queue.Queue()
 
 # Shared flag indicating whether to use ZeroMQ for sending messages
 use_zmq_for_sending = threading.Event()
@@ -61,10 +63,13 @@ def tcp_receive_thread(sock):
             data = sock.recv(1024)
             if data:
                 message = data.decode('utf-8')
-                if "entered album" in message:
-                    use_zmq_for_sending.set()
-                    tcp_send_queue.put("/getMetadata")
-                message_queue.put(message)
+                if use_zmq_for_sending.is_set():
+                    zmq_connect_queue.put(message)
+                else:
+                    if "entered album" in message:
+                        use_zmq_for_sending.set()
+                        tcp_send_queue.put("/getMetadata")
+                    message_queue.put(message)
             else:
                 print("Server disconnected.")
                 message_queue.put("exit")
@@ -76,41 +81,65 @@ def tcp_receive_thread(sock):
             tcp_send_queue.put("exit")
             break
 
-def zmq_dealer_thread(zmq_port):
+def zmq_pub_thread(context_pub, pub):
     """ ZeroMQ DEALER sends messages to the ROUTER. """
-    context = zmq.Context()
-    dealer = context.socket(zmq.DEALER)
-    dealer.connect("tcp://localhost:"+str(zmq_port))  # Connect to the ROUTER
 
-    while True:
+    # While context is not terminated
+    while context_pub:
         message = zmq_send_queue.get()
         if message == "exit":
-            dealer.close()
-            context.term()
-            print("Exiting zmq_dealer_thread.")
             use_zmq_for_sending.clear()
-            break
-        message = message + "\n"
-        dealer.send_string(message)
+        else:
+            message = message + "\n"
+            #send to all subscribers
+            pub.send_string(message)
 
-def zmq_router_thread(zmq_port):
+def zmq_sub_thread(context_sub, sub):
     """ ZeroMQ ROUTER receives messages from DEALER(s) and forwards them to output_thread. """
-    context = zmq.Context()
-    router = context.socket(zmq.ROUTER)
-    router.bind("tcp://*:" + str(zmq_port))  # Listen for DEALER connections
 
-    while True:
+    while context_sub:
         try:
-            _, message = router.recv_multipart()
-            if message.decode() == "exit":
-                router.close()
-                context.term()
-                print("Exiting zmq_router_thread.")
-                break
-            message_queue.put(message.decode())
+            message = sub.recv_string()
+            if message[0:3] == "tcp":
+                if message[-1] == "\n":
+                    message = message[:-1]
+                sub.disconnect(message)
+            else:
+                message_queue.put(message)
+            # message_queue.put(message.decode())
         except Exception as e:
             print(f"Failed to receive ZeroMQ message: {e}")
             break
+    print("Exiting zmq_sub_thread.")
+
+def zmq_connect(zmq_url,sub):
+    while True:
+        message = zmq_connect_queue.get()
+        # check if \n is at the end of the message and remove it
+        if message[-1] == "\n":
+            message = message[:-1]
+        if message == "exit":
+            break
+        elif message[0:3] == "tcp":
+            # remove \n from the end of the message
+            sub.connect(message)
+        else:
+            try:
+                message_json = json.loads(message)
+                for router in extract_routers(message_json):
+                    if zmq_url != router:
+                        sub.connect(router)
+            except Exception as e:
+                print(message)
+
+def extract_routers(data):
+    routers = []
+    users = data.get('users', {})
+    for _, info in users.items():
+        if info is not None and 'router' in info:
+            routers.append(info['router'])
+    return routers
+
 
 def main():
     if len(sys.argv) < 2:
@@ -119,11 +148,18 @@ def main():
     zmq_port = int(sys.argv[1])  # Get the port number from command line argument
     host = 'localhost'  # Server IP address
     port = 8000         # Server port
-    zmq_url = "tcp://localhost:" + str(zmq_port)
+    zmq_url = "tcp://127.0.0.1:" + str(zmq_port)
 
     # Create a shared socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
+    context_pub = zmq.Context().instance()
+    pub = context_pub.socket(zmq.PUB)
+    pub.bind("tcp://127.0.0.1:" + str(zmq_port))
+    context_sub = zmq.Context().instance()
+    sub = context_sub.socket(zmq.SUB)
+    sub.setsockopt(zmq.SUBSCRIBE, b"")
+
 
     # Start all threads
     threads = [
@@ -131,8 +167,9 @@ def main():
         threading.Thread(target=input_thread),
         threading.Thread(target=tcp_send_thread, args=(sock, zmq_url,)),
         threading.Thread(target=tcp_receive_thread, args=(sock,)),
-        threading.Thread(target=zmq_dealer_thread, args=(zmq_port,)),
-        threading.Thread(target=zmq_router_thread, args=(zmq_port,))
+        threading.Thread(target=zmq_pub_thread, args=(context_pub,pub,)),
+        threading.Thread(target=zmq_sub_thread, args=(context_sub,sub,)),
+        threading.Thread(target=zmq_connect, args=(zmq_url,sub,))
     ]
 
     for thread in threads:
@@ -147,7 +184,16 @@ def main():
         zmq_send_queue.put("exit")
         message_queue.put("exit")
     finally:
-        sock.close()
+        pub.send_string(zmq_url + "\n")
+        try:
+            pub.setsockopt(zmq.LINGER, 0)
+            pub.close()
+            sub.close()
+            context_pub.term()
+            context_sub.term()
+            sock.close()
+        except Exception as e:
+            print(f"Failed to close socket: {e}")
 
 if __name__ == "__main__":
     main()
